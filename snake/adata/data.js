277b610a8fdbbcf32343ebef4ca7c870b3c3f744
17681268574 <490047678@qq.com>
2025-06-20 22:24:24 +0800

    发布正式版Version 0.1.0620.3

--- snake/adata/data.js
+++ snake/adata/data.js
@@ -1,11 +1,66 @@
-
-window.v_type = 2; 
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: hjy
+ * @Date: 2025-05-29 11:25:02
+ * @LastEditors: hjy
+ * @LastEditTime: 2025-06-20 13:57:58
+ */
+window.v_type = 2; //0=QA；1=S0; 2=SN;
 
 //服务器path
 window.server2 = "https://sso.cryptoarena.io"; //正式环境
 window.wsServer2 = "wss://wss-hall-1.cryptoarena.io"; //正式环境
+// window.address2 = "https://snake.cryptoarena.io; //外网-sn-web地址 (线上正式)
 
-var nodeData = [   
+window.server1 = "https://sso.queengame.io"; //测试环境
+window.wsServer1 = "wss://wss-hall-1.queengame.io"; //测试环境
+// window.address1 = "https://snake.queengame.io"; //外网-s0-web地址 (线上测试)
+
+window.server0 = "http://13.250.6.10"; //开发环境
+window.wsServer0 = "ws://13.250.6.10:7055"; //开发服-qa-徐服务器
+
+var nodeData = [
+    {
+        area: 1010,
+        open: 1,
+        point: "1_00",
+        socketUrl: "ws://13.250.6.10:7056",
+        id: 3001,
+        name: 1010,
+        url: "https://wss-room-sg.queengame.io",
+        type: 0
+    },
+    {
+        area: 1010,
+        open: 1,
+        point: "1_00",
+        socketUrl: "wss://wss-room-sg.queengame.io",
+        id: 1001,
+        name: 1010,
+        url: "https://wss-room-sg.queengame.io",
+        type: 1
+    },
+    {
+        area: 1016,
+        open: 0,
+        point: "2_00",
+        socketUrl: "wss://wss-room-us.queengame.io",
+        id: 1002,
+        name: 1016,
+        url: "https://wss-room-us.queengame.io",
+        type: 1
+    },
+    {
+        area: 1018,
+        open: 0,
+        point: "3_00",
+        socketUrl: "wss://wss-room-jp.queengame.io",
+        id: 1003,
+        name: 1018,
+        url: "https://wss-room-jp.queengame.io",
+        type: 1
+    },
     {
         area: 1010,
         open: 1,
