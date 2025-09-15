b9f743bfd5350b2dc09b087971b71c70eecd235a
17681268574 <490047678@qq.com>
2025-09-15 19:17:34 +0800

    发布正式版Version 0.1.0915.12

--- snake/adata/data.js
+++ snake/adata/data.js
@@ -6,7 +6,7 @@
  * @LastEditors: hjy
  * @LastEditTime: 2025-09-03 11:05:47
  */
-window.v_type = 1; //0=QA；1=S0; 2=SN;
+window.v_type = 2; //0=QA；1=S0; 2=SN;
 
 //服务器path
 window.server2 = "https://sso.cryptoarena.io"; //正式环境
