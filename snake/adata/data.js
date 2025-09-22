a235cb0dda31f6238dbcc095826139c0267d1a9a
17681268574 <490047678@qq.com>
2025-09-22 21:19:20 +0800

    发布正式版

--- snake/adata/data.js
+++ snake/adata/data.js
@@ -1,9 +1,66 @@
-window.v_type = 2;
-window.server2 = "https://sso.cryptoarena.io";
-window.wsServer2 = "wss://wss-hall-1.cryptoarena.io";
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: hjy
+ * @Date: 2025-05-29 11:25:02
+ * @LastEditors: hjy
+ * @LastEditTime: 2025-09-19 17:16:54
+ */
+window.v_type = 2; //0=QA；1=S0; 2=SN;
 
+//服务器path
+window.server2 = "https://sso.cryptoarena.io"; //正式环境
+window.wsServer2 = "wss://wss-hall-1.cryptoarena.io"; //正式环境
+// window.address2 = "https://snake.cryptoarena.io; //外网-sn-web地址 (线上正式)
 
-var nodeData = [    
+window.server1 = "https://sso.candyhouse.vip"; //测试环境
+window.wsServer1 = "wss://wss-hall-1.candyhouse.vip"; //测试环境
+// window.address1 = "https://snake.candyhouse.vip"; //外网-s0-web地址 (线上测试)
+
+window.server0 = "http://13.250.6.10"; //开发环境
+window.wsServer0 = "ws://13.250.6.10:7055"; //开发服-qa-徐服务器
+
+var nodeData = [
+    {
+        area: 1010,
+        open: 1,
+        point: "1_00",
+        socketUrl: "ws://13.250.6.10:7056",
+        id: 3001,
+        name: 1010,
+        url: "https://wss-room-sg.candyhouse.vip",
+        type: 0
+    },
+    {
+        area: 1010,
+        open: 1,
+        point: "1_00",
+        socketUrl: "wss://wss-room-sg.candyhouse.vip",
+        id: 1001,
+        name: 1010,
+        url: "https://wss-room-sg.candyhouse.vip",
+        type: 1
+    },
+    {
+        area: 1016,
+        open: 0,
+        point: "2_00",
+        socketUrl: "wss://wss-room-us.candyhouse.vip",
+        id: 1002,
+        name: 1016,
+        url: "https://wss-room-us.candyhouse.vip",
+        type: 1
+    },
+    {
+        area: 1018,
+        open: 0,
+        point: "3_00",
+        socketUrl: "wss://wss-room-jp.candyhouse.vip",
+        id: 1003,
+        name: 1018,
+        url: "https://wss-room-jp.candyhouse.vip",
+        type: 1
+    },
     {
         area: 1010,
         open: 1,
