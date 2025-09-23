cffc326d8c5fbd6f8aeeadbfe0eb8f7fee44030c
hjy <545925654@qq.com>
2025-09-23 10:53:19 +0800

    1111

--- petcrush/js/Config-f6033.js
+++ petcrush/js/Config-f6033.js
@@ -4,9 +4,9 @@
  * @Author: hjy
  * @Date: 2025-04-22 14:02:12
  * @LastEditors: hjy
- * @LastEditTime: 2025-09-20 17:06:10
+ * @LastEditTime: 2025-09-23 10:35:55
  */
-var game_version = "Version 0.1.0922.1";
+var game_version = "Version 0.1.0923.2";
 
 //Buffer
 // var Buffer_1 = require("buffer");
