f5e9299d249385f53054d028a05ac40b32c58862
hjy <545925654@qq.com>
2025-09-25 14:13:03 +0800

    1111

--- snake/js/Config-995b7.js
+++ snake/js/Config-995b7.js
@@ -6,7 +6,7 @@
  * @LastEditors: hjy
  * @LastEditTime: 2025-09-24 17:02:28
  */
-var game_version = "Version 0.1.0924.04";
+var game_version = "Version 0.1.0925.01";
 
 //Buffer
 // var Buffer_1 = require("buffer");
