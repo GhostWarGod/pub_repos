0d6f6cba83bd782e1c2fc8cbca9a6ef46ce3ee94
hjy <545925654@qq.com>
2025-09-26 18:32:09 +0800

    11111

--- petcrush/js/Config-c9fc4.js
+++ petcrush/js/Config-c9fc4.js
@@ -4,9 +4,9 @@
  * @Author: hjy
  * @Date: 2025-04-22 14:02:12
  * @LastEditors: hjy
- * @LastEditTime: 2025-09-24 17:22:51
+ * @LastEditTime: 2025-09-26 10:59:36
  */
-var game_version = "Version 0.1.0925.2";
+var game_version = "Version 0.1.0926.2";
 
 //Buffer
 // var Buffer_1 = require("buffer");
