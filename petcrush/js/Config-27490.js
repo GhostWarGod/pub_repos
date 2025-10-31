14a641a6f96578fe832d4208ad7ae8793ad97fca
hjy <545925654@qq.com>
2025-10-31 11:19:51 +0800

    11111

--- petcrush/js/Config-27490.js
+++ petcrush/js/Config-27490.js
@@ -4,9 +4,9 @@
  * @Author: hjy
  * @Date: 2025-04-22 14:02:12
  * @LastEditors: hjy
- * @LastEditTime: 2025-10-15 14:11:22
+ * @LastEditTime: 2025-10-31 10:30:17
  */
-var game_version = "Version 0.1.1015.2";
+var game_version = "Version 0.1.1031.2";
 
 //Buffer
 // var Buffer_1 = require("buffer");
