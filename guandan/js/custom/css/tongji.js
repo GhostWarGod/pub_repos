590ed5c538ef086d37bc743a429467e7fd80b0e9
17681268574 <490047678@qq.com>
2025-11-26 20:25:33 +0800

    发布正式版掼蛋

---
+++ guandan/js/custom/css/tongji.js
@@ -0,0 +1,36 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: hjy
+ * @Date: 2024-04-09 13:06:24
+ * @LastEditors: hjy
+ * @LastEditTime: 2025-05-29 11:28:57
+ * @Description:
+ */
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?2f2e17b82f91b88a0a99db7ccba4eca4";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
+
+
+
+// var _hmt = _hmt || [];
+// (function() {
+//   var hm = document.createElement("script");
+//   hm.src = "https://hm.baidu.com/hm.js?0955f4cb5c089036ac304860376ed917";
+//   var s = document.getElementsByTagName("script")[0]; 
+//   s.parentNode.insertBefore(hm, s);
+// })();
+
+
+
+// window.dataLayer = window.dataLayer || [];
+// function gtag(){dataLayer.push(arguments);}
+// gtag('js', new Date());
+
+// gtag('config', 'G-B6F62P3M2K');
+// gtag('config', 'G-B6F62P3M2K', { 'debug_mode': true });
